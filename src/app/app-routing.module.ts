ad59c43ea24aac4a581fa084d2dc33822750ab69
Kian Woo <kian.woo@gmail.com>
2022-07-02 21:31:21 +1000

    feat: configure and link to remote

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'account-master', loadChildren: () => import('accountMaster/Module').then(m => m.AccountMasterModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
